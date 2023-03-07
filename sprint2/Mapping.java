85e717a5b0d7585a43e72b2d573ad0be34f3f40a
hasinjara <wenzelhasinjara@gmail.com>
2023-03-07 08:21:15 +0300

    test

---
+++ sprint2/Mapping.java
@@ -0,0 +1,17 @@
+package etu1919.framework;
+public class Mapping {
+    String className;
+    String method;
+    public String getClassName() {
+        return className;
+    }
+    public void setClassName(String className) {
+        this.className = className;
+    }
+    public String getMethod() {
+        return method;
+    }
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
